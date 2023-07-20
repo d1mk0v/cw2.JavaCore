e0a6d633ab08958bc5968829169d2842501decc9
d1mk0v <79874763588@ya.ru>
2023-07-20 11:22:40 +0500

    1

---
+++ src/test/java/pro/sky/questionService/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.questionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
