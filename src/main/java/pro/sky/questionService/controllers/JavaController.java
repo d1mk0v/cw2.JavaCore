e6de0ad00a4b94e882701be7e9c353feac8d671f
d1mk0v <79874763588@ya.ru>
2023-07-20 16:40:21 +0500

    4

---
+++ src/main/java/pro/sky/questionService/controllers/JavaController.java
@@ -0,0 +1,40 @@
+package pro.sky.questionService.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.questionService.services.api.Question;
+import pro.sky.questionService.services.api.QuestionService;
+import pro.sky.questionService.services.impl.JavaQuestionServiceImpl;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/exam/java")
+public class JavaController {
+    private final JavaQuestionServiceImpl javaQuestionService;
+
+    public JavaController(JavaQuestionServiceImpl javaQuestionService) {
+        this.javaQuestionService = javaQuestionService;
+    }
+
+    @GetMapping
+    public Collection<Question> getAll() {
+        return javaQuestionService.getAll();
+    }
+
+    @GetMapping(path = "/add")
+    public Question add(@RequestParam(name = "question") String question,
+                        @RequestParam(name = "answer") String answer) {
+        return javaQuestionService.add(question, answer);
+    }
+
+    @GetMapping(path = "/remove")
+    public Question remove(@RequestParam(name = "question") Question question) {
+        return javaQuestionService.remove(question);
+
+    }
+
+}
