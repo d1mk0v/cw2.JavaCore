f390e05ae5f8d592c873bd8205a6f0bc044e1ae9
d1mk0v <79874763588@ya.ru>
2023-07-20 20:40:00 +0500

    6

--- src/main/java/pro/sky/questionService/services/impl/ExaminerServiceImpl.java
+++ src/main/java/pro/sky/questionService/services/impl/ExaminerServiceImpl.java
@@ -1,13 +1,44 @@
 package pro.sky.questionService.services.impl;
 
-import pro.sky.questionService.services.impl.Question;
+import org.springframework.stereotype.Service;
+import pro.sky.questionService.exceptions.NotEnoughQuestionsException;
 import pro.sky.questionService.services.interfaces.ExaminerService;
+import pro.sky.questionService.services.interfaces.QuestionService;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
+@Service
 public class ExaminerServiceImpl implements ExaminerService {
+
+    private final QuestionService questionService;
+//    private Random random;
+
+    public ExaminerServiceImpl(QuestionService questionService) {
+        this.questionService = questionService;
+//        random = new Random();
+    }
+
     @Override
     public Collection<Question> getQuestions(int amount) {
-        return null;
+
+        Set<Question> listOfRandomQuestions = new HashSet<>();
+
+        while (listOfRandomQuestions.size() < amount) {
+            Question randomQuestion = questionService.getRandomQuestion();
+            listOfRandomQuestions.add(randomQuestion);
+
+            if (randomQuestion == null) {
+                break;
+            }
+        }
+
+        if (listOfRandomQuestions.size() < amount) {
+            throw new NotEnoughQuestionsException("Недостаточно вопросов!!!");
+        }
+
+        return listOfRandomQuestions;
     }
 }
