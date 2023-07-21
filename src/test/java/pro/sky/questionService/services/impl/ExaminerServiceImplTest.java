fa6f812c6e9c91649213d4b1b774f93c0dab8692
d1mk0v <130857934+d1mk0v@users.noreply.github.com>
2023-07-21 21:46:25 +0500

    Update src/test/java/pro/sky/questionService/services/impl/ExaminerServiceImplTest.java
    Co-authored-by: Ilya Savinov <il.savinov@gmail.com>

--- src/test/java/pro/sky/questionService/services/impl/ExaminerServiceImplTest.java
+++ src/test/java/pro/sky/questionService/services/impl/ExaminerServiceImplTest.java
@@ -28,7 +28,24 @@ public class ExaminerServiceImplTest {
     public void setup() {
         examinerService = new ExaminerServiceImpl(questionService);
     }
+ @Test
+public void getQuestionsTest() {
 
+        List<Question> questions = new ArrayList<>();
+
+        questions.add(new Question("Question 1", "Answer 1"));
+        questions.add(new Question("Question 2", "Answer 2"));
+        questions.add(new Question("Question 3", "Answer 3"));
+
+        when(questionService.getRandomQuestion()).thenReturn(questions.get(0)).thenReturn(questions.get(1));
+
+        int amount = 2;
+        Collection<Question> randomQuestions = examinerService.getQuestions(amount);
+
+        assertNotNull(randomQuestions);
+        assertEquals(amount, randomQuestions.size());
+        assertTrue(questions.containsAll(randomQuestions));
+    }
     @Test
     public void getQuestionsTest() {
 
